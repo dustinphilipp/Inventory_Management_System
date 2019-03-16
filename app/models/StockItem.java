d827f39da6c6ba9d4c548f08a4a5528ab042d4dd
Dustin Philipp <8943285+dustinphilipp@users.noreply.github.com>
2019-03-16 12:55:02 +0100

    Model modifications and additions
    Modified StockItem model with Path and QueryString binding and added the StockItemMovement model to log movement quantities and dates.

--- app/models/StockItem.java
+++ app/models/StockItem.java
@@ -2,21 +2,35 @@ package models;
 
 import io.ebean.Finder;
 import io.ebean.Model;
+import play.data.validation.Constraints;
+import play.mvc.PathBindable;
+import play.mvc.QueryStringBindable;
 
 import javax.persistence.Entity;
 import javax.persistence.Id;
 import javax.persistence.ManyToOne;
+import javax.persistence.OneToMany;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Map;
+import java.util.Optional;
 
 @Entity
-public class StockItem extends Model {
+public class StockItem extends Model implements PathBindable<StockItem>, QueryStringBindable<StockItem> {
 
   @Id
   public Long id;
   @ManyToOne
+  @Constraints.Required
   public Warehouse warehouse;
   @ManyToOne
+  @Constraints.Required
   public Product product;
+  @Constraints.Required
   public Long quantity;
+
+  @OneToMany(mappedBy="stockitem")
+  public List<StockItemMovement> stockMovement = new ArrayList<>();
 
   public static final Finder<Long, StockItem> find = new Finder<>(StockItem.class);
 
@@ -42,6 +56,36 @@ public class StockItem extends Model {
 
   public void setQuantity(Long quantity) {
     this.quantity = quantity;
+  }
+
+  public static StockItem findById(Long id) {
+    return find.query()
+        .where()
+        .eq("id", id)
+        .findOneOrEmpty()
+        .orElse(null);
+  }
+
+  @Override
+  public StockItem bind(String key, String value) {
+    System.out.println(Long.parseLong(value));
+    return findById(Long.parseLong(value));
+  }
+
+  @Override
+  public Optional<StockItem> bind(String key, Map<String, String[]> data) {
+    System.out.println(Optional.ofNullable(findById(Long.parseLong(data.get("id")[0]))));
+    return Optional.ofNullable(findById(Long.parseLong(data.get("id")[0])));
+  }
+
+  @Override
+  public String unbind(String s) {
+    return String.valueOf(this.id);
+  }
+
+  @Override
+  public String javascriptUnbind() {
+    return String.valueOf(this.id);
   }
 
   public String toString() {
---
+++ app/models/StockItemMovement.java
@@ -0,0 +1,105 @@
+package models;
+
+import io.ebean.Finder;
+import io.ebean.Model;
+import play.data.validation.Constraints;
+import play.mvc.PathBindable;
+import play.mvc.QueryStringBindable;
+
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.persistence.ManyToOne;
+import java.util.Date;
+import java.util.Map;
+import java.util.Optional;
+
+@Entity
+public class StockItemMovement extends Model implements PathBindable<StockItemMovement>, QueryStringBindable<StockItemMovement> {
+
+  @Id
+  public Long id;
+  @ManyToOne
+  @Constraints.Required
+  public StockItem stockItem;
+  @Constraints.Required
+  public Long movementQuantity;
+  @Constraints.Required
+  public Date movementDate;
+
+  public static final Finder<Long, StockItemMovement> find = new Finder<>(StockItemMovement.class);
+
+  public StockItemMovement() {}
+
+  public StockItemMovement(StockItem stockItem, Long movementQuantity, Date movementDate) {
+    this.stockItem = stockItem;
+    this.movementQuantity = movementQuantity;
+    this.movementDate = movementDate;
+  }
+
+  public Long getId() {
+    return id;
+  }
+
+  public void setId(Long id) {
+    this.id = id;
+  }
+
+  public StockItem getStockItem() {
+    return stockItem;
+  }
+
+  public void setStockItem(StockItem stockItem) {
+    this.stockItem = stockItem;
+  }
+
+  public Long getMovementQuantity() {
+    return movementQuantity;
+  }
+
+  public void setMovementQuantity(Long movementQuantity) {
+    this.movementQuantity = movementQuantity;
+  }
+
+  public Date getMovementDate() {
+    return movementDate;
+  }
+
+  public void setMovementDate(Date movementDate) {
+    this.movementDate = movementDate;
+  }
+
+  public static StockItemMovement findById(Long id) {
+    return find.query()
+        .where()
+        .eq("id", id)
+        .findOneOrEmpty()
+        .orElse(null);
+  }
+
+  @Override
+  public StockItemMovement bind(String key, String value) {
+    System.out.println(Long.parseLong(value));
+    return findById(Long.parseLong(value));
+  }
+
+  @Override
+  public Optional<StockItemMovement> bind(String key, Map<String, String[]> data) {
+    System.out.println(Optional.ofNullable(findById(Long.parseLong(data.get("id")[0]))));
+    return Optional.ofNullable(findById(Long.parseLong(data.get("id")[0])));
+  }
+
+  @Override
+  public String unbind(String s) {
+    return String.valueOf(this.id);
+  }
+
+  @Override
+  public String javascriptUnbind() {
+    return String.valueOf(this.id);
+  }
+
+  public String toString() {
+    return String.format("Stockitem movement %d, %d units on %s",
+                         this.id, this.movementQuantity, this.movementDate.toString());
+  }
+}
